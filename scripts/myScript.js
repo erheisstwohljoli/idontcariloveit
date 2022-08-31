76b109aa74e0e2a89f8eca184d4d54d33b12aa98
username138 <natnael.ghilamicael@uni-ulm.de>
2022-08-31 16:43:01 +0200

    freiburg map

--- scripts/myScript.js
+++ scripts/myScript.js
@@ -10,4 +10,8 @@ svg.attr('viewBox', [0, 0, canvas.clientWidth, canvas.clientHeight])
     .attr('height', canvas.clientHeight);
 
 
-
+const projection = d3.geoPath()
+    .scale((canvas.clientWidth - margin.left - margin.right) / (2* Math.PI))
+    .translate([canvas.clientWidth / 2, canvas.clientHeight / 2])
+    .center([0, 35]);
+const pathBuilder = d3.geoPath(projection);
